b2ab1f85350ae22347674ec7174d6c971911c653
Sean Donovan <sdonovan@gatech.edu>
2014-01-10 17:21:37 -0500

    Removing depricated code.

--- pyscraper.py
+++ pyscraper.py
@@ -4,29 +4,6 @@ import urllib2
 import httplib
 import urlparse
 
-def downloadSite(website):
-  print "Downloading", website
-  soup = BeautifulSoup(urllib2.urlopen(website))
-  imgs = soup.find_all('img')
-  imgcount = 0;
-  for img in imgs:
-    #print img
-    imglink = img['src']
-    #print imglink
-    if imglink[0] == '/':
-      imglink = website + imglink
-    print "  Image", imgcount, " link is ", imglink
-    urllib2.urlopen(imglink)
-    imgcount += imgcount
-  print "Finished   ", website
-
-
-def getMimeType2(address):
-  # This downloads the whole file from 'address', not useful for giant files.
-  maxlength = 1024*8
-  item = urllib2.urlopen(address)#.read(maxlength+1)
-  return item.info().gettype()
-
 def getMimeType(address):
   # Starting from https://stackoverflow.com/questions/1636637/i-am-downloading-a-file-using-python-urllib2-how-do-i-check-how-large-the-file
   u= urlparse.urlparse(address)
@@ -50,19 +27,6 @@ def getAllLinks(website):
 #  for page in pages:
 #    print page
   return pages
-
-def getLinksOfType2(website, soup, type, source):
-#dirty! Doesn't work so great
-  links = []
-  imgs = soup.find_all(type)
-  for img in imgs:
-    imglink = img[source]
-    print "imglink:", imglink
-    if imglink != "":
-      if imglink[0] == '/':
-        imglink = website + imglink
-      links.append(imglink)
-  return links
 
 def getLinksOfType(website, soup, type, source):
   links = []
